1913e46b0cffe791cbeaa0f149e53d0cfd9cf606
Emilio <Edwardestevez95@gmail.com>
2021-11-29 20:53:27 -0600

    pushing final final final

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout");
+const Workout = require("../models/workout");
 const path = require("path");
 const mongoose = require('mongoose')
 
